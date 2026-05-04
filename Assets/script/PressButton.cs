849175dcbd5b95134c6665675e9ab8697a3d2a08
MY-PERFECT-COOL\kobayashi akumu <xiaolint928@gmail.com>
2026-05-05 01:55:50 +0900

    AssetにてSEフォルダ作成、SE心音追加（小林）

--- Assets/script/PressButton.cs
+++ Assets/script/PressButton.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class PressButton : MonoBehaviour
 {
     public float speed = 2.0f; // 点滅の速さ
+    public bool isKeyPressed;
     private MaskableGraphic targetGraphic;
 
     void Start()
@@ -23,6 +25,11 @@ public class PressButton : MonoBehaviour
             Color color = targetGraphic.color;
             color.a = alpha;
             targetGraphic.color = color;
+        }
+        isKeyPressed = Keyboard.current != null && Keyboard.current.anyKey.wasPressedThisFrame;
+        if (isKeyPressed==true)
+        {
+            Debug.Log("いずれかのキーが押されました。");
         }
     }
 }
